f98412a7bd19a602ac43be29ed6d921cc4420090
tommyHellraiser <nacho.ponce25@gmail.com>
2023-10-18 02:20:46 -0300

    Started implementing script to read files names and classify them by extension

--- script.py
+++ script.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+import os
+import glob
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Tommy')
+    print("To start classifying your files by file type, please paste the folder path below:")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Getting the folder path
+    folder_path = "F:\\Descargas"
+
+    # Changing the current working directory
+    os.chdir(folder_path)
+
+    # Counting the items and folders to classify
+    items_in_folder = os.listdir(folder_path)
+    files_counter = 0
+    dirs_counter = 0
+    # print(items_in_folder)
+
+    for item in items_in_folder:
+        # TODO continue here
+        item.split(".")
+        # print(item)
+        # if os.path.isfile(os.path.join(folder_path, item)):
+        if os.path.isfile(item):
+            files_counter += 1
+        # elif os.path.isdir(os.path.join(folder_path, item)):
+        elif os.path.isdir(item):
+            dirs_counter += 1
+
+    # Awaiting user to accept the classification
+    print(f"There are {files_counter} files in the directory, and {dirs_counter} folders")
+    print("\nPress enter to start classifying by file extension or type exit to terminate program:")
+    if input() == "exit":
+        print("Exiting process...")
+        exit(0)
+
