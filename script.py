b868c6d6bd70bec92952fbe429547ea0522d847f
tponcePrex <tponce@prexcard.com.ar>
2023-10-18 12:59:07 -0300

    Script fully working. Thinking of improvements to make

--- script.py
+++ script.py
@@ -1,12 +1,26 @@
 import os
-import glob
+import shutil
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("To start classifying your files by file type, please paste the folder path below:")
 
     # Getting the folder path
-    folder_path = "F:\\Descargas"
+    # Desktop
+    # folder_path = "F:\\Descargas"
+
+    # Laptop
+    folder_path = "C:\\Users\\tponce\\Downloads"
+
+    # Organized files folder
+    organized_folder = folder_path + "\\organized"
+
+    if not os.path.exists(organized_folder):
+        os.mkdir(organized_folder)
+    else:
+        shutil.rmtree(organized_folder)
+        os.mkdir(organized_folder)
 
     # Changing the current working directory
     os.chdir(folder_path)
@@ -35,3 +49,22 @@ if __name__ == '__main__':
         print("Exiting process...")
         exit(0)
 
+    copy_count = 0
+
+    # Iterate and classify all items
+    for item in items_in_folder:
+        # Split into file name and extension
+        split = os.path.splitext(item)
+        # print(split[0])
+        extension = split[1][1::]
+
+        # If folder with extension name does not exist, create it
+        if not os.path.exists(extension) and extension != "":
+            os.mkdir(extension)
+
+        if extension != "":
+            result = shutil.copy(os.path.join(folder_path, item), extension)
+            if result is not None:
+                copy_count += 1
+
+    print(f"Successfully organized {copy_count} files")
